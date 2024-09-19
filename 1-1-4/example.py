3f9dbb88cbbe3d37e95708bedd0b0cb90c19b319
15020 <dylan.noah@vtsd.com>
2024-09-19 11:46:53 -0400

    first change

---
+++ 1-1-4/example.py
@@ -0,0 +1,9 @@
+
+num1 = int(input("Please Enter a number: "))
+num2 = int(input("Please Enter another number: "))
+if num1 > num2:
+    Print("number 1 was bigger")
+elif num2 > num1:
+    print("Number 2 was bigger")
+else:
+    print("Both numbers are the same")
---
+++ 1-1-4/grade.py
@@ -0,0 +1,6 @@
+grade = int(input("Please enter the grade you got no the last test"))
+if grade > 89:
+    print("A")
+if grade <79 and grade >89
+    print("B")
+    if grade
