bb3889eab9fbe6b4340205d41f2ef4b083752118
15020 <dylan.noah@vtsd.com>
2024-09-24 12:29:11 -0400

    first change

---
+++ 1-1-4/count_up.py
@@ -0,0 +1,5 @@
+total = 0
+
+while total + 11 <= 852:
+    total += 11
+print(total)
---
+++ 1-1-4/loops.py
@@ -0,0 +1,10 @@
+numm1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+
+while numm1 != num2:
+        print("You didnt enter the same number")
+        print("Please enter a valid response")
+        num1 = int(input('Enter a number: '))
+        num2 = int(input('Enter another number: '))
+
+print("You got it!")
