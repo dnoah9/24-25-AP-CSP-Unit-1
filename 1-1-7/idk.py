d3d5b22809d859b48c075bc1bfd16d107613b2c5
15020 <dylan.noah@vtsd.com>
2024-10-08 13:45:50 -0400

    first change

--- 1-1-7/idk.py
+++ 1-1-7/idk.py
@@ -10,22 +10,23 @@ turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
-  t = trtl.Turtle(shape=s)
-  my_turtles.append(t)
+    t = trtl.Turtle(shape=s)
+    my_turtles.append(t)
 
 #
-startx = 0
-starty = 0
+startx = 10
+starty = 10
 
 #
 for t in my_turtles:
-  t.goto(startx, starty)
-  t.right(50)
-  t.forward(50)
+    t.setheading(90)
+    t.goto(startx, starty)
+    t.right(45)
+    t.forward(50)
 
-#
-  startx = startx + 50
-  starty = starty + 50
+    #
+    startx = startx + 50
+    starty = starty + 50
 
 wn = trtl.Screen()
 wn.mainloop()
